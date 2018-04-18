b3dd3ec06d7dce85ff2247b0b62996ec61b729e4
Andy Iskauskas <andy.iskauskas@gmail.com>
2018-04-18 15:58:05 +0100

    Added (not entirely foolproof) node tree visualisation.

--- LogicNode.js
+++ LogicNode.js
@@ -13,6 +13,7 @@ class LogicNode
 		this.children = [];
 		this.unique = null;
 		this.level = null;
+		this.coords = [0,0];
 	}
 
 	get parent(){return this._parent}
@@ -50,11 +51,41 @@ class LogicNode
 		this._level = lvl;
 	}
 
+	get coords(){return this._coords}
+	set coords(val)
+	{
+		if (this.level != 0)
+		{
+			if (this.parent.expected() == 1)
+			{
+				this._coords = [this.parent.coords[0],this.parent.coords[1]+50];
+			}
+			else
+			{
+				var tempnode = this, offset = 1;
+				while (tempnode.parent != null)
+				{
+					tempnode = tempnode.parent;
+					offset *= 0.9;
+				}
+				if (this.parent.children[0].unique == this.unique)
+				{
+					this._coords = [this.parent.coords[0] - 100*offset, this.parent.coords[1] + 50];
+				}
+				else
+					this._coords = [this.parent.coords[0] + 100*offset, this.parent.coords[1] + 50];
+			}
+			return;
+		}
+		this._coords = val;
+	}
+
 	addChild(child)
 	{
 		this.children = child;
 		child.parent = this;
 		child.level = null;
+		child.coords = [0,0];
 	}
 
 	removeChild()
@@ -264,4 +295,42 @@ function truth_table(valArr, noofvals)
 	for (i=Math.pow(2,noofvals)-1; i>=0; i--)
 		outArr.push(valuation(valArr,int_to_binary_array(i,noofvals)));
 	return outArr;
+}
+
+function tree_to_canvas(valArr,cvs)
+{
+	var c = document.getElementById(cvs);
+	var ctx = c.getContext("2d");
+	var nodes = build_tree(valArr), i, j;
+	var maxright = 0, maxleft = 0, maxheight = 0, maxlevel = 0;
+	for (i=0; i<nodes.length; i++)
+	{
+		if (nodes[i].coords[0]>maxright)
+			maxright = nodes[i].coords[0];
+		if (nodes[i].coords[0]<maxleft)
+			maxleft = nodes[i].coords[0];
+		if (nodes[i].coords[1]>maxheight)
+			maxheight = nodes[i].coords[1];
+	}
+	c.width = maxright - maxleft + 100, c.height = maxheight+100;
+	var xoffset = -maxleft + 50, yoffset = 50;
+	for (i=0; i<nodes.length; i++)
+	{
+		var node = nodes[i];
+		var x = node.coords[0], y = node.coords[1];
+		for (j=0; j<node.children.length; j++)
+		{
+			ctx.moveTo(x+xoffset,y+yoffset);
+			ctx.lineTo(node.children[j].coords[0]+xoffset,node.children[j].coords[1]+yoffset);
+			ctx.fillStyle = "black";
+			ctx.stroke();
+		}
+		if (node.expected()==0)
+			var textoffset = 10;
+		else
+			var textoffset = -5;
+		ctx.textAlign = "center";
+		ctx.fillStyle = "blue";
+		ctx.fillText(node.value,x+xoffset,y+yoffset+textoffset);
+	}
 }
